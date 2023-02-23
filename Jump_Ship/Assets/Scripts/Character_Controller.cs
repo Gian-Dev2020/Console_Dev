b73840f072dd50182b4269f430d6cd2c347fc16b
Gian <72135815+Gian-Dev2020@users.noreply.github.com>
2023-02-23 13:53:38 +0000

    Camera - Work

---
+++ Jump_Ship/Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    CharacterController controller;
+    float rotation_X = 0f;
+    float rotation_Y = 0f;
+
+    [SerializeField] float speed;
+    [SerializeField] float sensitivity;
+    Vector2 turn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        rotation_Y += Input.GetAxis("Mouse X") * sensitivity;
+        rotation_X += Input.GetAxis("Mouse Y") * sensitivity;
+
+        this.transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+       
+       
+    }
+}
