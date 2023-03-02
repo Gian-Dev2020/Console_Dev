0206c60623851d9dbcbe29a5bac1cf68a77d01c1
James Wilson <jw9563@gmail.com>
2023-03-02 01:08:48 +0000

    update to ai

--- Jump_Ship/Assets/Scripts/AgentController.cs
+++ Jump_Ship/Assets/Scripts/AgentController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,12 @@ public class AgentController : MonoBehaviour
     [SerializeField] bool usePathPoints;
     [SerializeField] List<GameObject> pathPoints;
     [SerializeField] float waitTime = 2;
+
+    [SerializeField] LayerMask hitmask;
+    [SerializeField] float sightDistance = 10f;
+    [SerializeField] float sphereCastRadius = 2f;
     private int currentPathPoint = 0;
+    private RaycastHit info = new RaycastHit();
 
     public enum AgentType
     {
@@ -44,9 +50,19 @@ public class AgentController : MonoBehaviour
                 walking = false;
                 Invoke("UpdatePathPoint", waitTime);
             }
-        }   
+        }
+
+        Scan();
     }
 
+    private void Scan()
+    {        
+        if(Physics.SphereCast(new Ray(this.transform.position, this.transform.forward), sphereCastRadius, out info, sightDistance, hitmask, QueryTriggerInteraction.Ignore))
+        {
+
+            Debug.Log("Player has been seen by me:  " + agentType + "  " + this.gameObject.name);
+        }
+    }
 
     void UpdatePathPoint()
     {
@@ -59,4 +75,25 @@ public class AgentController : MonoBehaviour
         
         agent.SetDestination(pathPoints[currentPathPoint].transform.position);
     }
+
+
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.matrix *= Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        float length = sightDistance;
+
+        Gizmos.DrawWireSphere(Vector3.zero, sphereCastRadius);
+
+        Gizmos.DrawWireSphere(Vector3.zero + Vector3.forward * length, sphereCastRadius);
+        Gizmos.DrawLine(Vector3.up * sphereCastRadius, Vector3.up * sphereCastRadius + Vector3.forward * length);
+        Gizmos.DrawLine(Vector3.down * sphereCastRadius, Vector3.down * sphereCastRadius + Vector3.forward * length);
+        Gizmos.DrawLine(Vector3.left * sphereCastRadius, Vector3.left * sphereCastRadius + Vector3.forward * length);
+        Gizmos.DrawLine(Vector3.right * sphereCastRadius, Vector3.right * sphereCastRadius + Vector3.forward * length);
+    }
 }
+
